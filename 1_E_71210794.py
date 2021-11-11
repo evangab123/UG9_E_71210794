5d47a17c1ee2cf5a26d153b388f43c2ecc66de0c
EvanderGab <evander.gabriel@ti.ukdw.ac.id>
2021-11-11 15:46:27 +0700

    Tambahkan Soal 1_E_71210794

---
+++ 1_E_71210794.py
@@ -0,0 +1,7 @@
+alasatas = int(input("Masukan alas atas : "))
+alasbwh = int(input("Masukan alas bawah : "))
+tinggi = int(input("Masukan tinggi : "))
+
+luas = (( alasatas + alasbwh ) * 0.5) * tinggi
+
+print("Luas trapesium adalah : ", luas)
